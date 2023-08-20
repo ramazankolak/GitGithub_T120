f1bf09b32ecc17b7ef52018f83800a31bb07d84e
Ramazan Kolak <ramaznkolak@gmail.com>
2023-08-20 03:37:20 +0300

    20230820.v1.Ramazan

---
+++ src/GitStage.java
@@ -0,0 +1,6 @@
+public class GitStage {
+    public static void main(String[] args) {
+
+        System.out.println("");
+    }
+}
---
+++ src/Hello_Git.java
@@ -0,0 +1,9 @@
+public class Hello_Git {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+
+        // git add content root path ile sadece content root adresi girilen dosyayı gönderir.
+        // gitt add . tüm dosyaları stage area'ya atar
+    }
+}
